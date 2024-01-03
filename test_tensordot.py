7fa7617008d114003676fbe45a62876c8e0991cf
unknown <nzg.science@gmail.com>
2024-01-03 13:24:37 +0100

    update tensordot test for ninlayer

--- test_tensordot.py
+++ test_tensordot.py
@@ -4,16 +4,21 @@ import numpy as np
 
 # https://stackoverflow.com/questions/41870228/understanding-tensordot
 
-input_channels = 2
-num_units = 1
 
-input = torch.arange(90.).reshape(5, 2, 3, 3)
+# Testing the first NINLayer
+# -----------------------
+# incoming activations from the conv layer before
+# shape: (batch, out_features, width, height)
+input = torch.randn((5, 2, 3, 3))
+# NinLayer's weight shape (out_channels, in_features)
 W = torch.arange(20.).reshape(10, 2)
+# reduce dim along in_features axes
 c = torch.tensordot(W, input, dims=([1], [1]))
 print(c.shape)
-w = np.array(W)
 x = np.array(input)
-out_dim = (10,5,3,3)
+w = np.array(W)
+# out dim = (out_features, batch, width, height)
+out_dim = (10, 5, 3, 3)
 el = np.zeros((out_dim))
 for i in range(10):
     for j in range(5):
@@ -21,14 +26,54 @@ for i in range(10):
             for q in range(3):
                 for l in range(2):
                     el[i,j,k,q] += w[i, l] * x[j,l,k,q]
+
 print(c.shape)
 print(el.shape)
 
-# out_r = c
-# print(input.ndim)
-# remaining_dims = range(2, input.ndim)
-# print(remaining_dims)
-# # bf01...
-# print(out_r.shape)
-# out = out_r.permute(1, 0, *remaining_dims)
-# print(out.shape)
+# --- reshuffle the dimension of the output to match shape (batch, out_features, width, height)
+out_r = c
+#print(input.ndim)
+remaining_dims = range(2, input.ndim)
+#print(remaining_dims)
+# bf01...
+#print(out_r.shape)
+out = out_r.permute(1, 0, *remaining_dims)
+# (batch, out_features, width, height)
+print(out.shape)
+
+
+# Testing the second NINLayer
+# -----------------------
+# incoming activations from the NINLayer before
+# shape: (batch, out_features, width, height)
+input = out
+# second NinLayer's weight shape (out_channels, in_features)
+W = torch.randn((192, 10))
+# reduce dim along in_features axes
+c = torch.tensordot(W, input, dims=([1], [1]))
+print(c.shape)
+w = np.array(W)
+x = np.array(input)
+out_dim = (192, 5, 3, 3)
+el = np.zeros((out_dim))
+for i in range(192):
+    for j in range(5):
+        for k in range(3):
+            for q in range(3):
+                for l in range(2):
+                    el[i,j,k,q] += w[i, l] * x[j,l,k,q]
+
+print(c.shape)
+print(el.shape)
+
+# --- reshuffle the dimension of the output to match shape (batch, out_features, width, height)
+out_r = c
+#print(input.ndim)
+remaining_dims = range(2, input.ndim)
+#print(remaining_dims)
+# bf01...
+#print(out_r.shape)
+out = out_r.permute(1, 0, *remaining_dims)
+# rearrange to have shape to have shape: (batch, out_features, width, height)
+# (batch, out_features, width, height)
+print(out.shape)
